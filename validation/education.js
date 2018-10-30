5559da72f89aaa374a7632a37f9ff71d0fdb7cd5
Nevena Stoeva <nevena.stoeva@gmail.com>
2018-10-30 00:01:13 -0700

    Adds education validation

---
+++ validation/education.js
@@ -0,0 +1,29 @@
+const Validator = require("validator");
+const isEmpty = require("./is-empty");
+
+module.exports = function validateExperienceInputData(data) {
+    let errors = {};
+    
+    data.school = !isEmpty(data.school) ? data.school : '';
+    data.degree = !isEmpty(data.degree) ? data.degree : '';
+    data.fieldOfStudy = !isEmpty(data.fieldOfStudy) ? data.fieldOfStudy : '';
+    data.from = !isEmpty(data.from) ? data.from : '';
+
+    if (Validator.isEmpty(data.school)) {
+        errors.school = "Please include your school"
+    }
+    if (Validator.isEmpty(data.degree)) {
+        errors.degree = "Please include your degree"
+    }
+    if (Validator.isEmpty(data.fieldOfStudy)) {
+        errors.fieldOfStudy = "Please include your field of study"
+    }
+    if (Validator.isEmpty(data.from)) {
+        errors.from = "Please include a starting date"
+    }
+
+    return {
+        errors,
+        isValid: isEmpty(errors)
+    }
+};
